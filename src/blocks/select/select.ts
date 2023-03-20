01a85549a9523e543ca20a6bf8796f9e824466b3
Vlad <makrak.vladislav@yandex.ru>
2023-03-21 01:10:31 +0300

    feat: add custom styles for select

---
+++ src/blocks/select/select.ts
@@ -0,0 +1,6 @@
+//import "nativejs-select/build/nativejs-select.css";
+
+const NativejsSelect = require("nativejs-select");
+new NativejsSelect({
+  selector: ".form__select",
+});
--- src/js/index.ts
+++ src/js/index.ts
@@ -1,2 +1,3 @@
 import "../blocks/input/input";
 import "../blocks/range-slider/range-slider";
+import "../blocks/select/select";
